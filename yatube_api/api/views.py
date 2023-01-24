d204419121b5c733906482db4d4fd824ff3cdbf2
Евгений Минеев <ew.mineew@ya.ru>
2023-01-24 09:35:56 +0800

    Перед третьим ревью

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Group, Post
 from rest_framework import filters, permissions, viewsets
 
 from .permissions import AuthorPermission
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
+from posts.models import Group, Post
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
